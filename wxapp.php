796851bb776bb89dcef962283c480fe1d0a54dcf
itxiaolong3 <1710600973@qq.com>
2019-02-28 11:57:07 +0800

    改了支付流水号长度，varchar(100)查询有点问题

--- wxapp.php
+++ wxapp.php
@@ -872,7 +872,7 @@ class maskModuleWxapp extends WeModuleWxapp {
         $openid=$_GPC['openid'];//oQKgL0ZKHwzAY-KhiyEEAsakW5Zg
         $mch_id=$res['mchid'];
         $key=$res['wxkey'];
-        $out_trade_no = $mch_id. time();
+        $out_trade_no = rand(111,999).date('YmdHis',time());//订单号
         $root=$_W['siteroot'];
         pdo_update('mask_order',array('code'=>$out_trade_no),array('id'=>$_GPC['orderid']));
         $total_fee =$_GPC['money'];
